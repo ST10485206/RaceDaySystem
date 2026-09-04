fa1f1eb54358d849198fa04d925d1c0344c78bd3
ST10485206 <matshwaneboikhutso25@gmail.com>
2026-09-04 16:14:42 +0200

    Create Organiser Table

--- docs/RaceDay_Database.sql
+++ docs/RaceDay_Database.sql
@@ -5,3 +5,13 @@ GO
 
 USE RaceDay;
 GO
+CREATE TABLE Organiser
+(
+    OrganiserID INT IDENTITY(1,1) PRIMARY KEY,
+    Firstname VARCHAR(50) NOT NULL,
+    Lastname VARCHAR(50) NOT NULL,
+    Email VARCHAR(100) NOT NULL,
+    Password VARCHAR(255) NOT NULL,
+    Phonenumber VARCHAR(20) NOT NULL
+);
+GO
