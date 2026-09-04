1c622fd4590993d7a45734a7818fedc5de2c0fe9
ST10485206 <matshwaneboikhutso25@gmail.com>
2026-09-04 16:37:26 +0200

    Create  Event table

--- docs/RaceDay_Database.sql
+++ docs/RaceDay_Database.sql
@@ -15,3 +15,17 @@ CREATE TABLE Organiser
     Phonenumber VARCHAR(20) NOT NULL
 );
 GO
+
+CREATE TABLE Event
+(
+    EventID INT IDENTITY(1,1) PRIMARY KEY,
+    EventName NVARCHAR(100) NOT NULL,
+    Location NVARCHAR(100) NOT NULL,
+    [Date] DATE NOT NULL,
+    OrganiserID INT NOT NULL,
+
+    CONSTRAINT FK_Event_Organiser
+        FOREIGN KEY (OrganiserID)
+        REFERENCES Organiser(OrganiserID)
+);
+GO
